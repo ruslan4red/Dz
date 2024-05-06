56c42467d06389e36ecd60f960392452a1cd3937
Ruslan_Budyakov <ordo.red.esp@gmail.com>
2024-05-06 19:16:08 +0300

    lesson 2, task 3-4

---
+++ Lesson2_task3.cpp
@@ -0,0 +1,23 @@
+/*Оригінал:
+ 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << Is there a bug here?"
+    return 0;
+    }    
+*/
+
+
+// Оновлений код:
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Is there a bug here?"; // Були відсутні лапки та крапка з комою
+	return 0;
+}    
